fa0d13bb13f3c43008b1c0babccb4a17a4ad824a
Baptiste Lefeuvre <baptiste.lefeuvre@outlook.fr>
2023-09-04 22:37:07 +0200

    Fix docker healthyness

--- alti/main.py
+++ alti/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.post("/alti/")
 def get_alti(lonlat: list[tuple[float, float]]) -> list[int]:
     return get_alti_from_lonlat(lonlat)
+
+@app.get("/")
+def check():
+    # Juste return success code 200
+    return {"status": "ok"}
